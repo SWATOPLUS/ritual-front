0f36ab54bca0b89fa7baa201b31abe8de33132a1
RobXela <78446731+RobXela@users.noreply.github.com>
2022-02-02 19:10:59 +0300

    [5] Adjusted font at main page

--- components/Steps/index.tsx
+++ components/Steps/index.tsx
@@ -1,36 +1,35 @@
-import { Col, Container, Row } from 'react-bootstrap'
-import Image from 'next/image'
-import styles from './style.module.scss'
+import { Col, Container, Row } from "react-bootstrap";
+import Image from "next/image";
+import styles from "./style.module.scss";
 
 export interface StepsProps {
-    steps: Array<StepType>
+  steps: Array<StepType>;
 }
 
 export type StepType = {
-    icon: string,
-    text: string
-}
+  icon: string;
+  text: string;
+};
 
-export const Steps = ({steps}:StepsProps) => {
-    return (
-        <div className={styles.steps}>
-            <Container className={styles.steps_container}>
-                <h1 className={styles.title}>{steps.length} шага использования нашей услуги</h1>
-                <Row xs={2} md={4}>
-                    {steps.map((step, ind)=> {
-                        return (
-                            <Col key={ind} >
-                                <Image src={step.icon} width={100} />
-                                <p>
-                                    {step.text}
-                                </p>
-                            </Col>
-                        )
-                    })}
-                    
-                </Row>
-            </Container>
-        </div>
-
-    )
-}
+export const Steps = ({ steps }: StepsProps) => {
+  return (
+    <div className={styles.steps}>
+      <Container className={styles.steps_container}>
+        <h1 className={styles.title}>
+          <span className={styles.number}>{steps.length}</span> шага
+          использования нашей услуги
+        </h1>
+        <Row xs={2} md={4}>
+          {steps.map((step, ind) => {
+            return (
+              <Col key={ind}>
+                <Image src={step.icon} width={100} />
+                <p>{step.text}</p>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+};
