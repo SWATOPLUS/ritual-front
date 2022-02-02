a3ed2e5ea7698031b17748d527bb72d785daa8b5
RobXela <78446731+RobXela@users.noreply.github.com>
2022-02-02 07:05:08 +0300

    [6] Added content in RecoverModal Component

--- components/Modals/RecoverModal/RecoverModal.tsx
+++ components/Modals/RecoverModal/RecoverModal.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import VerificationInput from "react-verification-input";
 import { restorePassReq } from "../../../api/auth";
 import { useModalContext } from "../../../contexts/modals";
+import Breadcrumb from "../../Breadcrumb";
+import { InputModal } from "../../Forms/Inputs/InputsModal";
+import styles from "./style.module.scss"
 
 export const RecoverModal = () => {
     const [id, setId] = useState(0);
@@ -14,6 +18,10 @@ export const RecoverModal = () => {
     }
 
     return <>
+    <Breadcrumb defaultItem={0}> 
+        <Breadcrumb.Item>Восстановление пароля</Breadcrumb.Item>
+    </Breadcrumb>
+    
     <RecoverEnterEmail hidden={!!id} onSubmit={(x) => setId(x)}/>
     <RecoverEnterCode hidden={!!code || !id} onSubmit={(x) => setCode(x)}/>
     <RecoverEnterPassword hidden={!!pass || !code} onSubmit={(x) => setPass(x)}/>
@@ -22,24 +30,34 @@ export const RecoverModal = () => {
 
 const RecoverEnterEmail = (props: PageProps<number>) => {
     return (
-        <div hidden={props.hidden}>
-            <span>Введите E-mail</span>
-            <button onClick={() => props.onSubmit(42)}>Получить код</button>
+        <div hidden={props.hidden} className={styles.content}>
+            <p>Введите почту на который должен прийти код подтверждения</p>
+            <input type="text" placeholder="Введите E-mail"/>
+            <button className={styles.btn} onClick={() => props.onSubmit(42)}>Получить код</button>
         </div>
     )
 }
 
 const RecoverEnterCode = (props: PageProps) => {
-    return <div hidden={props.hidden}>
-    <span>Введите Код</span>
-    <button onClick={() => props.onSubmit('code')}>Отправить</button>
+    return <div hidden={props.hidden} className={styles.content}>
+    <p>Введите код, который пришел на почту</p>
+    <VerificationInput
+        placeholder=" "
+        validChars="0-9"
+        removeDefaultStyles={true}
+        classNames={{
+        container: `${styles.container}`,
+        character: `${styles.character}`,
+}}/>
+    <button className={styles.btn} onClick={() => props.onSubmit('code')}>Отправить</button>
     </div>
 }
 
 const RecoverEnterPassword = (props: PageProps) => {
-    return <div hidden={props.hidden}>
-    <span>Введите новый пароль</span>
-    <button onClick={() => props.onSubmit('password')}>Отправить</button>
+    return <div hidden={props.hidden} className={styles.content}>
+    <p>Введите новый пароль</p>
+    <InputModal placeholder="Новый пароль" password={true}/>
+    <button className={styles.btn} onClick={() => props.onSubmit('password')}>Отправить</button>
     </div>
 }
 
